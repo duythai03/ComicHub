b640dee1c819beaa8bab539e47d441b3fbc9d91c
duythai03 <148519165+duythai03@users.noreply.github.com>
2024-11-26 19:28:08 +0700

    fix bottom tab

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -5,8 +5,9 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import SearchScreen from "../screens/SearchScreen";
 import ProfileScreen from "../screens/ProfileScreen";
-import FontAwesome6 from "react-native-vector-icons/FontAwesome6";
-import GenreScreen from "../screens/GenreScreen";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import ExploreScreen from "@/screens/Explore/ExploreScreen";
+import FavouriteScreen from "@/screens/FavouriteScreen";
 import ComicScreen from "../screens/ComicScreen";
 import ReadingScreen from "../screens/ReadingScreen";
 import { useTheme } from "../utils/Context";
@@ -20,83 +21,86 @@ const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 export default function AppNavigation() {
-	const { isDarkMode } = useTheme();
+  const { isDarkMode } = useTheme();
 
-	const theme = isDarkMode ? darkTheme : lightTheme;
+  const theme = isDarkMode ? darkTheme : lightTheme;
 
-	function HomeStack() {
-		return (
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-				}}
-				initialRouteName="LoginScreen"
-			>
-				<Stack.Screen name="HomeTab" component={HomeTab} />
-				<Stack.Screen name="Welcome" component={WelcomeScreen} />
-				<Stack.Screen name="LoginScreen" component={LoginScreen} />
-				<Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-				<Stack.Screen
-					name="Search"
-					component={SearchScreen}
-					options={{ headerShown: true }}
-				/>
-				<Stack.Screen name="ComicStack" component={ComicStack} />
-			</Stack.Navigator>
-		);
-	}
+  function HomeStack() {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="LoginScreen"
+      >
+        <Stack.Screen name="HomeTab" component={HomeTab} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+        <Stack.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ headerShown: true }}
+        />
+        <Stack.Screen name="ComicStack" component={ComicStack} />
+      </Stack.Navigator>
+    );
+  }
 
-	function ComicStack({ route }) {
-		return (
-			<Stack.Navigator
-				screenOptions={{
-					headerShown: false,
-				}}
-				initialRouteName="Comic"
-			>
-				<Stack.Screen
-					name="Comic"
-					component={ComicScreen}
-					initialParams={route?.params}
-				/>
-				<Stack.Screen name="Reading" component={ReadingScreen} />
-			</Stack.Navigator>
-		);
-	}
+  function ComicStack({ route }) {
+    return (
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Comic"
+      >
+        <Stack.Screen
+          name="Comic"
+          component={ComicScreen}
+          initialParams={route?.params}
+        />
+        <Stack.Screen name="Reading" component={ReadingScreen} />
+      </Stack.Navigator>
+    );
+  }
 
-	function HomeTab() {
-		return (
-			<Tab.Navigator
-				screenOptions={({ route }) => ({
-					headerShown: false,
-					tabBarStyle: { backgroundColor: theme.container.backgroundColor },
-					tabBarActiveTintColor: "#c226f1",
-					tabBarInactiveTintColor: theme.text.color,
-					tabBarIcon: ({ color, size }) => {
-						let iconName;
+  function HomeTab() {
+    return (
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          headerShown: false,
+          tabBarStyle: { backgroundColor: theme.container.backgroundColor },
+          tabBarActiveTintColor: "#c226f1",
+          tabBarInactiveTintColor: theme.text.color,
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
 
-						if (route.name === "Home") {
-							iconName = "house";
-						} else if (route.name === "Genre") {
-							iconName = "book-open";
-						} else if (route.name === "Profile") {
-							iconName = "user";
-						}
+            if (route.name === "Home") {
+              iconName = "home";
+            } else if (route.name === "Favourite") {
+              iconName = "heart";
+            } else if (route.name === "Explore") {
+              iconName = "compass";
+            } else if (route.name === "Profile") {
+              iconName = "user";
+            }
 
-						return <FontAwesome6 name={iconName} size={size} color={color} />;
-					},
-				})}
-			>
-				<Tab.Screen name="Home" component={HomeScreen} />
-				<Tab.Screen name="Genre" component={GenreScreen} />
-				<Tab.Screen name="Profile" component={ProfileScreen} />
-			</Tab.Navigator>
-		);
-	}
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Explore" component={ExploreScreen} />
+        <Tab.Screen name="Favourite" component={FavouriteScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    );
+  }
 
-	return (
-		<NavigationContainer ref={navigationRef}>
-			<HomeStack />
-		</NavigationContainer>
-	);
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <HomeStack />
+    </NavigationContainer>
+  );
 }
